d6c7439aa87bd782a2a98b2140d727eea29f5214
Shino1024 <justincompspec@gmail.com>
2015-07-22 20:03:05 +0200

    Began to work on the tree data structure.

---
+++ treer.go
@@ -0,0 +1,20 @@
+package treer
+
+import (
+//	"fmt"
+)
+
+type Tree struct {
+	root   *node
+	length int
+	depth  int
+}
+
+func InitializeTree(d interface{}) *Tree {
+	
+}
+
+func InitializeTreeA(d []interface{}) *Tree {
+	
+}
+
