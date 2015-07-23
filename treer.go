2b7411c410a6e8fbf72fb668169e825c7f9cdccc
Shino1024 <justincompspec@gmail.com>
2015-07-23 23:07:11 +0100

    Further work on the tree initialization.

--- treer.go
+++ treer.go
@@ -2,6 +2,7 @@ package treer
 
 import (
 //	"fmt"
+	"reflect"
 )
 
 type Tree struct {
@@ -10,8 +11,35 @@ type Tree struct {
 	depth  int
 }
 
+type node struct {
+	left  *node
+	right *node
+	data  {}interface
+}
+
 func InitializeTree(d interface{}) *Tree {
-	
+	ret := new(Tree)
+	ret.root.left = nil
+	ret.root.right = nil
+	temp := reflect.ValueOf(d)
+	if temp.Len() == 0 {
+		ret.root = nil
+		ret.length = 0
+		ret.depth = 0
+		return ret
+	}
+	temp2 := make([]interface{}, temp.Len())
+	for i := 0; i < temp.Len(); i++ {
+		temp2[i] = temp.IndexOf(i).Interface()
+	}
+	for i := 0; i < temp.Len(); i++ {
+		temp3 := new(node)
+		temp3 = ret.root
+		temp3.data = temp2[i]
+		for temp3.left != nil || temp3.right != nil {
+			if 
+		}
+	}
 }
 
 func InitializeTreeA(d []interface{}) *Tree {
